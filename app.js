80fa4ebd095db06a313444d15bce5b1f975f8212
meza68k <meza24k@gmail.com>
2022-08-02 16:34:39 -0500

    inqirer works

---
+++ app.js
@@ -0,0 +1,33 @@
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
+
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username'
+      },
+      {
+        type: 'input',
+        name: 'about',
+        message: 'Provide some information about yourself:'
+      }
+    ]);
+  
+  };
+promptUser().then(answers => console.log(answers));
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
